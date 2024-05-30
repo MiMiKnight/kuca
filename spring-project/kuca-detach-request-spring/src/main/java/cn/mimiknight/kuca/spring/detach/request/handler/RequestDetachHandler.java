7af972da81a9dedf9ce6307cc413d296256cef91
MiMiKnight <victor2015yhm@gmail.com>
2024-05-30 23:23:28 +0800

    [Requirement Number]US202308182020 [Commit Description]删除kuca-detach-request-spring

--- spring-project/kuca-detach-request-spring/src/main/java/cn/mimiknight/kuca/spring/detach/request/handler/RequestDetachHandler.java
+++
@@ -1,6 +0,0 @@
-package cn.mimiknight.kuca.spring.detach.request.handler;
-
-import cn.mimiknight.kuca.proto.detach.handler.WithParamWithReturnDetachHandler;
-
-public interface RequestDetachHandler<Q, P> extends WithParamWithReturnDetachHandler<Q, P> {
-}
