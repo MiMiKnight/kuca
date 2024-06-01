f9ee4382f23d168ca1c779720b748444f79375cc
MiMiKnight <victor2015yhm@gmail.com>
2024-06-01 16:14:21 +0800

    [Requirement Number]US202308182020 [Commit Description]kuca-errorcode代码

---
+++ proto-project/kuca-errorcode/src/main/java/cn/mimiknight/kuca/proto/errorcode/ErrorCodeManager.java
@@ -0,0 +1,12 @@
+package cn.mimiknight.kuca.proto.errorcode;
+
+/**
+ * error code manager
+ *
+ * @author MiMiKnight victor2015yhm@gmail.com
+ * @date 2024-06-01 15:56:18
+ * @since v1.0
+ */
+public class ErrorCodeManager {
+
+}
---
+++ proto-project/kuca-errorcode/src/main/java/cn/mimiknight/kuca/proto/errorcode/ErrorCodeUtil.java
@@ -0,0 +1,16 @@
+package cn.mimiknight.kuca.proto.errorcode;
+
+public final class ErrorCodeUtil {
+
+    private ErrorCodeUtil() {
+    }
+
+    /**
+     * check error code
+     *
+     * @param code code
+     */
+    public static void check(String code) {
+
+    }
+}
