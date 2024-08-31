37d7d55d16b1f19a851d9ac7d50a31bc64cfb0a0
MiMiKnight <victor2015yhm@gmail.com>
2024-08-31 22:31:16 +0800

    [Requirement Number]US202308182020 [Commit Description]优化kuca-ecology-boot-starter

--- spring-boot-project/kuca-spring-boot-autoconfigure/src/main/java/cn/mimiknight/kuca/boot/autoconfigure/ecology/DetachRequestAutoConfiguration.java
+++
@@ -1,9 +0,0 @@
-package cn.mimiknight.kuca.boot.autoconfigure.ecology;
-
-import org.springframework.boot.autoconfigure.AutoConfiguration;
-import org.springframework.boot.context.properties.EnableConfigurationProperties;
-
-@AutoConfiguration
-@EnableConfigurationProperties(value = {DetachRequestProperties.class})
-public class DetachRequestAutoConfiguration {
-}
---
+++ spring-boot-project/kuca-spring-boot-autoconfigure/src/main/java/cn/mimiknight/kuca/boot/autoconfigure/ecology/EcologyAutoConfiguration.java
@@ -0,0 +1,32 @@
+package cn.mimiknight.kuca.boot.autoconfigure.ecology;
+
+import cn.mimiknight.kuca.spring.ecology.EcologyConfig;
+import cn.mimiknight.kuca.spring.ecology.EcologyManager;
+import cn.mimiknight.kuca.spring.ecology.EcologyManagerFactory;
+import org.springframework.boot.autoconfigure.AutoConfiguration;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+
+/**
+ * ecology auto configuration
+ *
+ * @author MiMiKnight victor2015yhm@gmail.com
+ * @date 2024-08-31 22:08:56
+ * @since v1.0
+ */
+@AutoConfiguration
+@EnableConfigurationProperties(value = {EcologyProperties.class})
+public class EcologyAutoConfiguration {
+
+    @Bean("ecologyManager")
+    @ConditionalOnMissingBean
+    public EcologyManager getEcologyManager(EcologyProperties properties) {
+        EcologyManager manager = EcologyManagerFactory.create();
+        EcologyConfig config = manager.getConfig();
+        config.setRequestValidation(EcologyMapStruct.INSTANCE.convert(properties.getRequestValidation()));
+        config.setResponseValidation(EcologyMapStruct.INSTANCE.convert(properties.getResponseValidation()));
+        manager.setConfig(config);
+        return manager;
+    }
+}
