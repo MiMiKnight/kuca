5cde480337eed2ca8d19e869705c2875d97e40cf
MiMiKnight <victor2015yhm@gmail.com>
2024-09-02 21:21:11 +0800

    [Requirement Number]US202308182020 [Commit Description]优化

--- spring-boot-project/kuca-spring-boot-autoconfigure/src/main/java/cn/mimiknight/developer/kuca/boot/autoconfigure/ecology/EcologyAutoConfiguration.java
+++ spring-boot-project/kuca-spring-boot-autoconfigure/src/main/java/cn/mimiknight/developer/kuca/boot/autoconfigure/ecology/EcologyAutoConfiguration.java
@@ -3,10 +3,14 @@ package cn.mimiknight.developer.kuca.boot.autoconfigure.ecology;
 import cn.mimiknight.developer.kuca.spring.ecology.EcologyConfig;
 import cn.mimiknight.developer.kuca.spring.ecology.EcologyManager;
 import cn.mimiknight.developer.kuca.spring.ecology.EcologyManagerFactory;
+import cn.mimiknight.developer.kuca.spring.ecology.EcologyParamValidationProcessor;
+import cn.mimiknight.developer.kuca.spring.ecology.EcologyRequestExecutor;
+import cn.mimiknight.developer.kuca.spring.ecology.handler.EcologyRequestHandlerBox;
 import org.springframework.boot.autoconfigure.AutoConfiguration;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Import;
 
 /**
  * ecology auto configuration
@@ -16,6 +20,8 @@ import org.springframework.context.annotation.Bean;
  * @since v1.0
  */
 @AutoConfiguration
+@Import(value = {EcologyRequestExecutor.class, EcologyRequestHandlerBox.class,
+        EcologyParamValidationProcessor.class})
 @EnableConfigurationProperties(value = {EcologyProperties.class})
 public class EcologyAutoConfiguration {
 
